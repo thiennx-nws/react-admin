e30352c48494523d10f37d8057ed39be67d981cd
thiennx <thiennx@newwave.vn>
2019-01-30 15:13:42 +0700

    fix export formMiddleWare

--- packages/ra-core/src/form/index.js
+++ packages/ra-core/src/form/index.js
@@ -1,15 +1,17 @@
 import addField from './addField';
 import FormDataConsumer from './FormDataConsumer';
 import FormField from './FormField';
+import formMiddleware from './formMiddleware';
 import getDefaultValues from './getDefaultValues';
 import withDefaultValue from './withDefaultValue';
 
 export {
-    addField,
-    FormDataConsumer,
-    FormField,
-    getDefaultValues,
-    withDefaultValue,
+  addField,
+  FormDataConsumer,
+  FormField,
+  formMiddleware
+  getDefaultValues,
+  withDefaultValue,
 };
 export { isRequired } from './FormField';
 export * from './validate';
